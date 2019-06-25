67d859e07608990322df14da69654c2c0fd6dcb5
Yohannes Kifle <ykifle2@gmail.com>
2019-06-25 00:42:24 -0700

    Weird

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
+    if true
+      puts 'hi'
+    end
     render text: "¡Hola, mundos!"
   end
 
