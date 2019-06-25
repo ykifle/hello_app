adede186fccbab2b57d3e9913f8210549e3c03bf
Yohannes Kifle <ykifle2@gmail.com>
2019-06-25 00:17:27 -0700

    Add bad spelling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "¡Hola, mundo!"
+    render text: "¡Hola, mundos!"
   end
 
   def goodbye
